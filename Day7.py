7785267b1ce0b35d0c60dbf318b67ee3287ef4bc
Josh Ethridge <joshua.ethridge13@gmail.com>
2017-12-07 16:44:09 -0500

    Day 7 part 1

--- Day7.py
+++ Day7.py
@@ -1,9 +1,35 @@
 import unittest
 
 def calc(data):
+	d = treeify(data)
+	n = d.itervalues().next()
+	while n.parent != None:
+		n = n.parent
+	return n.name
+
+def treeify(data):
+	d = {}
 	for i in data:
-		print(i)
-	return
+		i = i.replace('\n', '')
+		i = i.split(' -> ')
+		i[0] = i[0].split(' ')
+		i[0][1] = int(i[0][1].replace('(', '').replace(')', ''))
+		if len(i) > 1:
+			i[1] = i[1].split(', ')
+		if d.get(i[0][0]) == None:
+			n = Node(i[0][0], i[0][1])
+			n.weight = i[0][1]
+			d[i[0][0]] = n
+		else:
+			d.get(i[0][0]).weight = i[0][1]
+		if len(i) <= 1:
+			continue
+		for j in i[1]:
+			if d.get(j) == None:
+				n = Node(j)
+				d[j] = n
+			d.get(j).parent = d.get(i[0][0])
+	return d
 
 def load(path):
 	data = []
@@ -12,7 +38,12 @@ def load(path):
 			data.append(line)
 	return data
 
+class Node(object):
 
+	def __init__(self, name, weight=-1):
+		self.name = name
+		self.weight = weight
+		self.parent = None
 
 class TestDay7(unittest.TestCase):
 
@@ -22,7 +53,7 @@ class TestDay7(unittest.TestCase):
 
 if __name__ == '__main__':
 	unittest.main()
-	# Part 1:
+	# Part 1: ykpsek
 	print(calc(load('Day7.txt')))
 	#Part 2:
 	print(calc(load('Day7.txt')))
