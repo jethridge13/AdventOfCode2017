f06e645bf638ff0308cdf7ad8a777ea0d42cd994
Joshua Ethridge <joshua.ethridge13@gmail.com>
2017-12-09 14:25:21 -0500

    Day 9 Part 1 Done, Part 2 WIP

---
+++ Day9.py
@@ -0,0 +1,112 @@
+import unittest
+
+def calcScore(line):
+	stack = []
+	score = 0
+	garbage = False
+	skip = False
+	for i in range(len(line)):
+		if skip:
+			skip = False
+			continue
+		if not garbage:
+			if line[i] == '{':
+				stack.append('{')
+			elif line[i] == '}':
+				score += len(stack)
+				stack.pop()
+			elif line[i] == '<':
+				garbage = True
+		else:
+			if line[i] == '>':
+				garbage = False
+			elif line[i] == '!':
+				skip = True
+	return score
+
+def calcGarbage(line):
+	stack = []
+	score = 0
+	garbage = False
+	skip = False
+	for i in range(len(line)):
+		if skip:
+			skip = False
+			continue
+		if not garbage:
+			if line[i] == '{':
+				stack.append('{')
+			elif line[i] == '}':
+				score += len(stack)
+				stack.pop()
+			elif line[i] == '<':
+				garbage = True
+		else:
+			if line[i] == '>':
+				garbage = False
+			elif line[i] == '!':
+				skip = True
+			else:
+				score += 1
+	return score
+
+def load(path):
+	data = ''
+	with open(path, 'r') as f:
+		data = f.read()
+	return data
+
+class TestDay9(unittest.TestCase):
+
+	def test1(self):
+		t = '{}'
+		self.assertEqual(calcScore(t), 1)
+
+	def test2(self):
+		t = '{{{}}}'
+		self.assertEqual(calcScore(t), 6)
+		t = '{{},{}}'
+		self.assertEqual(calcScore(t), 5)
+		t = '{{{},{},{{}}}}'
+		self.assertEqual(calcScore(t), 16)
+
+	def test3(self):
+		t = '{<a>,<a>,<a>,<a>}'
+		self.assertEqual(calcScore(t), 1)
+		t = '{{<ab>},{<ab>},{<ab>},{<ab>}}'
+		self.assertEqual(calcScore(t), 9)
+
+	def test4(self):
+		t = '{{<!!>},{<!!>},{<!!>},{<!!>}}'
+		self.assertEqual(calcScore(t), 9)
+		t = '{{<a!>},{<a!>},{<a!>},{<ab>}}'
+		self.assertEqual(calcScore(t), 3)
+
+	def test5(self):
+		t = '<>'
+		self.assertEqual(calcGarbage(t), 0)
+
+	def test6(self):
+		t = '<random characters>'
+		self.assertEqual(calcGarbage(t), 17)
+		t = '<<<<>'
+		self.assertEqual(calcGarbage(t), 3)
+
+	def test7(self):
+		t = '<{!>}>'
+		self.assertEqual(calcGarbage(t), 2)
+		t = '<!!>'
+		self.assertEqual(calcGarbage(t), 0)
+		t = '<!!!>>'
+		self.assertEqual(calcGarbage(t), 0)
+
+	def test8(self):
+		t = '<{o"i!a,<{i<a>'
+		self.assertEqual(calcGarbage(t), 10)
+
+if __name__ == '__main__':
+	#unittest.main()
+	# Part 1: 10050
+	print(calcScore(load('Day9.txt')))
+	# Part2: 14532
+	print(calcGarbage(load('Day9.txt')))
