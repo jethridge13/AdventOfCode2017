a429bf2d30fb481dd5b58d816bce1ae032a62176
Josh Ethridge <joshua.ethridge13@gmail.com>
2017-12-12 13:41:48 -0500

    Day 10 Part 2

--- Day10.py
+++ Day10.py
@@ -45,6 +45,16 @@ def calc2(l, lengths, skipSize=0, index=0):
 		skipSize += 1
 	return l[0] * l[1], index, skipSize
 
+def calc2Main(l, lengths):
+	index = 0
+	skipSize = 0
+	lengths = appendSequence(lengths)
+	for i in range(64):
+		check, index, skipSize = calc2(l, lengths, skipSize, index)
+	hsh = sparseToDense(l)
+	return hsh
+
+
 def load(path):
 	data = ''
 	with open(path, 'r') as f:
@@ -55,6 +65,7 @@ def loadString(path):
 	data = ''
 	with open(path, 'r') as f:
 		data = f.read()
+	data = data.replace('\n', '')
 	return data
 
 def transformIntoASCII(s):
@@ -67,8 +78,13 @@ def appendSequence(s):
 	return transformIntoASCII(s) + [17, 31, 73, 47, 23]
 
 def sparseToDense(l):
-	# TODO
-	return
+	# Given the 256 numbers, break into 16 blocks
+	data = []
+	for i in range(16):
+		n = i * 16
+		x = xorBlock(l[n:n+16])
+		data.append(x)
+	return toHex(data)
 
 def xorBlock(l):
 	x = l[0]
@@ -113,9 +129,28 @@ class TestDay10(unittest.TestCase):
 	def test7(self):
 		t = [64, 7, 255]
 		self.assertEqual(toHex(t), '4007ff')
+
+	def test8(self):
+		t = ''
+		self.assertEqual(calc2Main(list(range(0, 256)), t), 'a2582a3a0e66e6e86e3812dcb672a272')
+
+	def test9(self):
+		t = 'AoC 2017'
+		self.assertEqual(calc2Main(list(range(0, 256)), t), '33efeb34ea91902bb2f59c9920caa6cd')
+
+	def test10(self):
+		t = '1,2,3'
+		self.assertEqual(calc2Main(list(range(0, 256)), t), '3efbe78a8d82f29979031a4aa0b16a9d')
+		t = '1,2,4'
+		self.assertEqual(calc2Main(list(range(0, 256)), t), '63960835bcdc130f0b66d7ff4f6a5a8e')
 	
+	def test11(self):
+		t = loadString('Day10.txt')
+		self.assertEqual(t, '106,16,254,226,55,2,1,166,177,247,93,0,255,228,60,36')
+
 if __name__ == '__main__':
-	unittest.main()
+	#unittest.main()
 	# Part 1: 11413
 	print(calc(list(range(0, 256)), load('Day10.txt')))
-	# Part 2:
+	# Part 2: 7adfd64c2a03a4968cf708d1b7fd418d
+	print(calc2Main(list(range(0, 256)), loadString('Day10.txt')))
